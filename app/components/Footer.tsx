98c5cd46352e74564c08c65263cdbe96b1c3624b
maxlee <maxplee8@gmail.com>
2024-09-15 02:41:17 -0400

    ☹️ couldnt finish stats thing

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {GrGithub} from "react-icons/gr";
+import {doc, getDoc} from "@firebase/firestore";
+import {db} from "@/firebase";
 
 export const Footer = () => {
+  useEffect(() => {
+    // get total games played
+    const fetchTotalGamesPlayed = async () => {
+      const docRef = doc(db, "stats", "stats");
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Total games played: ", docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
+    };
+    fetchTotalGamesPlayed();
+
+  }, []);
   return (
     <div
       className={"flex-col text-sm text-gray-400 font-mono px-4 mt-20 self-end flex justify-center items-center w-full"}>
-      <a href={"https://github.com/max-lee-dev/smashwhosbetterat"} className="flex items-center">
-        <GrGithub className="text-black text-4xl"/>
+      <div className={"flex-col items-center flex"}>
+        <a href={"https://github.com/max-lee-dev/smashwhosbetterat"} className="flex items-center">
+          <GrGithub className="text-black text-4xl"/>
 
 
-      </a>
-      <p className="pt-2 text-center">by <a href={"https://youtube.com/@Spenkr"}
-                                            className={"text-blue-500"}>Spenkr</a>&apos;s</p>
-      <p>
-        lil bro &gt;:)</p>
+        </a>
+        <p className="pt-2 text-center">by <a href={"https://youtube.com/@Spenkr"}
+                                              className={"text-blue-500"}>Spenkr</a>&apos;s</p>
+        <p>lil bro &gt;:)</p>
+      </div>
+      {/*<div className={"self-end"}>*/}
+      {/*  <p>2021</p>*/}
+      {/*</div>*/}
+
     </div>
   );
 };
